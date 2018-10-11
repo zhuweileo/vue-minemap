4048747175cc64f117cd3d8a1c215e9fefd7cb5d
cennavi <zhuwei_leo@163.com>
2018-10-11 15:01:58 +0800

    新增：计划使用vuepress写文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'vue-minemap',
+  description: 'Just playing around',
+  themeConfig:{
+    // displayAllHeaders: true,
+    sidebar: [
+      ['/','首页'],
+      '/page-a',
+      '/demo/',
+    ]
+  }
+};
