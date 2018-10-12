a343393e49d443e5fd7ec3cde9f48f70beaa7afc
cennavi <zhuwei_leo@163.com>
2018-10-12 15:25:25 +0800

    修复bug: 导出getMap方法

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as MMLayer} from './component/MMLayer'
 export {default as MMSource} from './component/MMSource'
 export {default as MineMap} from './component/MineMap'
+export {getMap} from './component/MineMap'
