4237e6d9deda86c1201f89d7b3d678476fe15b22
cennavi <zhuwei_leo@163.com>
2018-10-17 15:34:47 +0800

    新增：文档中新增gitHub地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,18 @@ module.exports = {
   title: 'vue-minemap',
   description: 'use minemap in vue',
   dest: 'vuepress',
-  base:'/vue-minemap/vuepress/',
-  head:[
-    ['link',{rel: 'stylesheet',href: '/lib/minemap2.0/minemap.css'}],
-    ['script',{src: '/lib/minemap2.0/minemap.js'}],
+  base: '/vue-minemap/vuepress/',
+  head: [
+    ['link', {rel: 'stylesheet', href: '/lib/minemap2.0/minemap.css'}],
+    ['script', {src: '/lib/minemap2.0/minemap.js'}],
   ],
   themeConfig: {
     displayAllHeaders: true,
     nav: [
-      { text: '指引', link: '/guide/' },
-      { text: '文档', link: '/api/' },
-      { text: '示例', link: '/demo/basic-map' },
+      {text: '指引', link: '/guide/'},
+      {text: '文档', link: '/api/'},
+      {text: '示例', link: '/demo/basic-map'},
+      {text: 'GitHub', link: 'https://github.com/zhuweileo/vue-minemap'},
     ],
     sidebar: {
       '/demo/': [
